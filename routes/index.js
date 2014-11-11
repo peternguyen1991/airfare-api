c9722ba5aaaabfafa2cd5305c4cdc911cd2ae814
Peter Nguyen <duc.nguyen@puresolutions.com.vn>
2014-11-11 17:00:06 +0700

    2

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
+var request = require('request');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/airfares/page/:page', function(req, res){
+	var page = req.params.page;
+	var url = 'http://128.199.222.42:3000/api';
+	request({url: url}, function (error, response, body){
+		var json = JSON.parse(body);
+	}
+	res.render('index', { title: 'Express', json: json });
+})
 
 module.exports = router;
